7de8556a3cfbc11eb1e2f9783a376d6473c40eb8
Vakhurin Sergey <igelbox@gmail.com>
2019-01-05 03:03:18 +0300

    Add TM1637 LED display driver

---
+++ README.md
@@ -0,0 +1,58 @@
+# `TM1639`
+> A platform agnostic driver to a LED-display powered by the TM1637 chip
+
+## What works
+- Displaying raw bits and HEX-digits
+- Controlling brightness
+
+## TODO
+
+## Example
+Here is a simple example for using the MAX7219 on a stm32f103xx device with stm32f103xx_hal:
+```rust
+#![deny(unsafe_code)]
+#![no_std]
+
+extern crate cortex_m;
+extern crate stm32f103xx_hal as hal;
+
+use hal::delay::Delay;
+use hal::prelude::*;
+use hal::stm32f103xx::Peripherals;
+
+extern crate tm1637;
+use tm1637::{TM1637};
+
+fn main() {
+
+    let dp = Peripherals::take().unwrap();
+
+    let mut rcc = dp.RCC.constrain();
+
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let mut clk = gpiob.pb6.into_open_drain_output(&mut gpiob.crl);
+    let mut dio = gpiob.pb7.into_open_drain_output(&mut gpiob.crl);
+
+    let mut flash = dp.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let cp = cortex_m::Peripherals::take().unwrap();
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    let mut tm = TM1637::new(&mut clk, &mut dio, &mut delay);
+    tm.init().unwrap();
+    tm.clear().unwrap();
+    loop {
+        for i in 0..255 {
+            tm.print_hex(0, &[i, i + 1]).unwrap();
+
+            tm.print_raw(3, &[i]).unwrap();
+
+            tm.set_brightness(i >> 5).unwrap();
+        }
+    }
+}
+```
+
+## License
+Licensed under MIT license ([LICENSE](LICENSE) or http://opensource.org/licenses/MIT)
---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+#![deny(warnings)]
+#![no_std]
+
+extern crate embedded_hal as hal;
+
+use hal::blocking::delay::DelayUs;
+use hal::digital::{InputPin, OutputPin};
+
+#[derive(Debug)]
+pub enum Error {
+    Ack,
+}
+
+pub struct TM1637<'a, CLK, DIO, D> {
+    clk: &'a mut CLK,
+    dio: &'a mut DIO,
+    delay: &'a mut D,
+}
+
+impl<'a, CLK, DIO, D> TM1637<'a, CLK, DIO, D>
+where
+    CLK: OutputPin,
+    DIO: InputPin + OutputPin,
+    D: DelayUs<u16>,
+{
+    pub fn new(clk: &'a mut CLK, dio: &'a mut DIO, delay: &'a mut D) -> Self {
+        Self { clk, dio, delay }
+    }
+
+    pub fn init(&mut self) -> Result<(), Error> {
+        self.start();
+        self.send(ADDRESS_AUTO_INCREMENT_1_MODE)?;
+        self.stop();
+        Ok(())
+    }
+
+    pub fn clear(&mut self) -> Result<(), Error> {
+        self.print_raw_iter(0, core::iter::repeat(0).take(4))
+    }
+
+    pub fn print_raw(&mut self, address: u8, bytes: &[u8]) -> Result<(), Error> {
+        self.print_raw_iter(address, bytes.iter().map(|b| *b))
+    }
+
+    pub fn print_hex(&mut self, address: u8, digits: &[u8]) -> Result<(), Error> {
+        self.print_raw_iter(
+            address,
+            digits.iter().map(|digit| DIGITS[(digit & 0xf) as usize]),
+        )
+    }
+
+    pub fn print_raw_iter<Iter: Iterator<Item = u8>>(
+        &mut self,
+        address: u8,
+        bytes: Iter,
+    ) -> Result<(), Error> {
+        self.start();
+        self.send(ADDRESS_COMMAND_BITS | (address & ADDRESS_COMMAND_MASK))?;
+        for byte in bytes {
+            self.send(byte)?;
+        }
+        self.stop();
+        Ok(())
+    }
+
+    pub fn set_brightness(&mut self, level: u8) -> Result<(), Error> {
+        self.start();
+        self.send(DISPLAY_CONTROL_BRIGHTNESS_BITS | (level & DISPLAY_CONTROL_BRIGHTNESS_MASK))?;
+        self.stop();
+        Ok(())
+    }
+
+    fn send(&mut self, byte: u8) -> Result<(), Error> {
+        let mut rest = byte;
+        for _ in 0..8 {
+            self.clk.set_low();
+            if rest & 1 != 0 {
+                self.dio.set_high()
+            } else {
+                self.dio.set_low()
+            }
+            rest = rest >> 1;
+            self.clk.set_high();
+            self.delay();
+        }
+
+        // Wait for the ACK
+        self.clk.set_low();
+        self.dio.set_high();
+        self.clk.set_high();
+        for _ in 0..255 {
+            if !self.dio.is_high() {
+                return Ok(());
+            }
+            self.delay();
+        }
+
+        Err(Error::Ack)
+    }
+
+    fn start(&mut self) {
+        self.clk.set_low();
+        self.dio.set_high();
+        self.clk.set_high();
+        self.delay();
+        self.dio.set_low();
+    }
+
+    fn stop(&mut self) {
+        self.clk.set_low();
+        self.dio.set_low();
+        self.clk.set_high();
+        self.delay();
+        self.dio.set_high();
+        self.delay();
+    }
+
+    fn delay(&mut self) {
+        self.delay.delay_us(DELAY_USECS);
+    }
+}
+
+const MAX_FREQ_KHZ: u16 = 250;
+const USECS_IN_MSEC: u16 = 1_000;
+const DELAY_USECS: u16 = USECS_IN_MSEC / MAX_FREQ_KHZ;
+
+const ADDRESS_AUTO_INCREMENT_1_MODE: u8 = 0x40;
+
+const ADDRESS_COMMAND_BITS: u8 = 0xc0;
+const ADDRESS_COMMAND_MASK: u8 = 0x0f;
+
+const DISPLAY_CONTROL_BRIGHTNESS_BITS: u8 = 0x88;
+const DISPLAY_CONTROL_BRIGHTNESS_MASK: u8 = 0x07;
+
+const DIGITS: [u8; 16] = [
+    0x3f, 0x06, 0x5b, 0x4f, //
+    0x66, 0x6d, 0x7d, 0x07, //
+    0x7f, 0x6f, 0x77, 0x7c, //
+    0x39, 0x5e, 0x79, 0x71, //
+];
